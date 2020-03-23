9c673905c43af84fa5f60483f449c01fb278246a
Sem8 <mssemmi8@gmail.com>
2020-03-23 19:56:34 -0400

    staging commit

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -3,57 +3,71 @@ import { withRouter } from "react-router";
 
 import axios from "../axios-instance";
 
+import episodes from './sylefiles/episodes.scss';
+
 const Episodes = props => {
   const { seasonId } = props;
-//   console.log('seasonId type : ', typeof seasonId);
-//   let seasonIdNum = seasonId.toString();
+  //   console.log('seasonId type : ', typeof seasonId);
+  //   let seasonIdNum = seasonId.toString();
   const [episodes, setEpisodes] = useState([]);
 
-//   useEffect(() => {
-//     axios.get(`seasons/${seasonId}/episodes`).then(res => {
-//       console.log("res seasons: ", res);
-//       setEpisodes(res.data);
-//     });
-//   }, []);
+  // useEffect(() => {
+  //   axios.get(`seasons/${seasonId}/episodes`).then(res => {
+  //     console.log("res seasons: ", res);
+  //     setEpisodes(res.data);
+  //   });
+  // }, []);
 
-let config = {
-  headers: {'Access-Control-Allow-Origin': '*'}
-};
+  let config = {
+    headers: { "Access-Control-Allow-Origin": "*" }
+  };
 
-let getEpisodes = () => {
+  let getEpisodes = () => {
     axios
-    .get(`seasons/${seasonId}/episodes`, config)
-    .then(res => {
-      // console.log("fuzzy search shows", res);
-      setEpisodes(res.data);
-      // console.log('suggestions: ', suggestions);
-      // console.log('iso Date: ', new Date(suggestions[0].show.premiered));
-    })
-    .catch(err => {
-      console.log(err.message);
-    });
-}
+      .get(`seasons/${seasonId}/episodes`)
+      .then(res => {
+        // console.log("fuzzy search shows", res);
+        setEpisodes(res.data);
+        // console.log('episodes: ', episodes)
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  let renderEpisodeTooltip = () => {
+    
+  }
 
   return (
     <>
       <div>List of episodes</div>
       {getEpisodes()}
-
+      <div className='listOfEpisodes'>
       {episodes &&
         episodes.map(eachEpisode => {
           return (
             <>
-              <div>
-                <div>
-                  <img src={eachEpisode.image.medium} />
-                  <p>{eachEpisode.name}</p>
-                  <p>{eachEpisode.summary}</p>
+              <div className="episodes" key={eachEpisode.id}>
+                <div className="episode-tooltip">
+                  <img
+                    src={eachEpisode.image.medium}
+                    className="episode-tooltip-img"
+                  />
+                  <p className="episode-tooltip-name">{eachEpisode.name}</p>
+                  <p className="episode-tooltip-summary">
+                    {eachEpisode.summary}
+                  </p>
                 </div>
-                Episode {eachEpisode.number}
+                <div className="episodeNumber">
+                  <div className='episodeCircle'></div>
+                  Episode {eachEpisode.number}
+                </div>
               </div>
             </>
           );
         })}
+        </div>
     </>
   );
 };
